fdf4167de2b60e31471e68c9dc69e35727094509
Daniel Hrivnak <dhrivnak@gmail.com>
2025-08-25 08:43:37 -0500

    obfuscate Stark project token in accessibility test

--- Stark iOS SampleUITests/Stark_iOS_SampleUITests.swift
+++ Stark iOS SampleUITests/Stark_iOS_SampleUITests.swift
@@ -19,8 +19,7 @@ final class Stark_iOS_SampleUITests: XCTestCase {
                 app.launch()
 
                 // Create a checker and audit the screen
-                setenv("STARK_API_URL", "http://localhost:3000/api/automated-scan/result/ios", 1)
-                let checker = AccessibilityChecker(starkProjectToken: "stark_1452216e48434f07bd0cf4f6f6502e56")
+                let checker = AccessibilityChecker(starkProjectToken: "stark_••••••••••••••••••••••••••••••••")
                 try checker.auditScreen(application: app, scanName: "ios-instrumentation")
     }
 }
